a3dbb86a39033e4ddd6b5de53231b6dcf706d74a
Jatin KrishanKumar Sharma <jatin.sharma@joshtechnologygroup.com>
2020-02-04 18:09:40 +0530

    MI-796 make constants for event message

--- server/service/confluence_cloud.go
+++ server/service/confluence_cloud.go
@@ -8,6 +8,15 @@ import (
 	"github.com/mattermost/mattermost-server/model"
 )
 
+const (
+	pageCreateMessage = "Page [%s](%s) created in space **%s**"
+	commentCreateMessage= "[Comment](%s) created in page [%s](%s)"
+	pageUpdateMessage = "Page [%s](%s) updated in space **%s**"
+	commentUpdateMessage= "[Comment](%s) updated in page [%s](%s)"
+	pageDeleteMessage = "Page [%s](%s) removed in space **%s**"
+	commentDeleteMessage= "Comment deleted in page [%s](%s)"
+)
+
 func SendConfluenceCloudNotification(event *serializer.ConfluenceCloudEvent, eventType string) {
 	post := generateConfluenceCloudNotificationPost(event, eventType)
 	if post == nil {
@@ -27,17 +36,17 @@ func generateConfluenceCloudNotificationPost(event *serializer.ConfluenceCloudEv
 	comment := event.Comment
 	switch eventType {
 	case "page_created":
-		message = fmt.Sprintf("Page [%s](%s) created in space **%s**", page.Title, page.Self, page.SpaceKey)
+		message = fmt.Sprintf(pageCreateMessage, page.Title, page.Self, page.SpaceKey)
 	case "comment_created":
-		message = fmt.Sprintf("[Comment](%s) created in page [%s](%s)", comment.Self, comment.Parent.Title, comment.Parent.Self)
+		message = fmt.Sprintf(commentCreateMessage, comment.Self, comment.Parent.Title, comment.Parent.Self)
 	case "page_update":
-		message = fmt.Sprintf("Page [%s](%s) updated in space **%s**", page.Title, page.Self, page.SpaceKey)
+		message = fmt.Sprintf(pageUpdateMessage, page.Title, page.Self, page.SpaceKey)
 	case "comment_update":
-		message = fmt.Sprintf("[Comment](%s) updated in page [%s](%s)", comment.Self, comment.Parent.Title, comment.Parent.Self)
+		message = fmt.Sprintf(commentUpdateMessage, comment.Self, comment.Parent.Title, comment.Parent.Self)
 	case "page_removed":
-		message = fmt.Sprintf("Page [%s](%s) created in space **%s**", page.Title, page.Self, page.SpaceKey)
+		message = fmt.Sprintf(pageDeleteMessage, page.Title, page.Self, page.SpaceKey)
 	case "comment_delete":
-		message = fmt.Sprintf("Comment deleted in page [%s](%s)", comment.Parent.Title, comment.Parent.Self)
+		message = fmt.Sprintf(commentDeleteMessage, comment.Parent.Title, comment.Parent.Self)
 	default:
 		return nil
 	}
