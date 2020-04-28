740abdcb1deee29dbfff0f11758cf36f6ae5f535
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2020-04-28 13:33:55 -0400

    handle webapp plugin crash

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,13 +4,22 @@ import reducer from './reducers';
 
 import SubscriptionModal from './components/subscription_modal';
 
+const displayNameError = 'You attempted to set the key `display_name` with the value `""` on an object that is meant to be immutable and has been frozen.';
+
 //
 // Define the plugin class that will register
 // our plugin components.
 //
 class PluginClass {
     initialize(registry, store) {
-        registry.registerReducer(reducer);
+        try {
+            registry.registerReducer(reducer);
+        } catch (e) {
+            // If it is the display name error, ignore. Otherwise re-throw error.
+            if (!e.toString().includes(displayNameError)) {
+                throw e;
+            }
+        }
         registry.registerRootComponent(SubscriptionModal);
         const hooks = new Hooks(store);
         registry.registerSlashCommandWillBePostedHook(hooks.slashCommandWillBePostedHook);
