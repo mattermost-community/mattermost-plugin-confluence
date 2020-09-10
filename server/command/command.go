d9a5b34dcb4bf4c9d669bb82c379b5ac5b7e303b
Jason Frerich <jason.frerich@mattermost.com>
2020-09-10 15:33:34 -0500

    Move Sysadmin commands in help section

--- server/command/command.go
+++ server/command/command.go
@@ -24,12 +24,12 @@ const (
 	subscriptionDeleteSuccess = "Subscription **%s** has been deleted."
 	noChannelSubscription     = "No subscriptions found for this channel."
 	commonHelpText            = "###### Mattermost Confluence Plugin - Slash Command Help\n\n" +
-		"* `/confluence subscribe` - Subscribe the current channel to notifications from Confluence.\n" +
-		"* `/confluence unsubscribe \"<name>\"` - Unsubscribe the current channel from notifications associated with the given subscription name.\n" +
-		"* `/confluence list` - List all subscriptions for the current channel.\n" +
-		"* `/confluence edit \"<name>\"` - Edit the subscription settings associated with the given subscription name.\n"
+		"* `/confluence list` - List all subscriptions for the current channel.\n"
 
 	sysAdminHelpText = "\n###### For System Administrators:\n" +
+		"* `/confluence subscribe` - Subscribe the current channel to notifications from Confluence.\n" +
+		"* `/confluence unsubscribe \"<name>\"` - Unsubscribe the current channel from notifications associated with the given subscription name.\n" +
+		"* `/confluence edit \"<name>\"` - Edit the subscription settings associated with the given subscription name.\n" +
 		"Setup Instructions:\n" +
 		"* `/confluence install cloud` - Connect Mattermost to a Confluence Cloud instance.\n" +
 		"* `/confluence install server` - Connect Mattermost to a Confluence Server or Data Center instance.\n"
